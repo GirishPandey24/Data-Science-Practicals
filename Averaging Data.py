1c56e3a6c816c36bdd4fabdbfdb81c7826ed52f2
GirishPandey24 <55070765+GirishPandey24@users.noreply.github.com>
2022-12-06 04:07:25 +0530

    Add files via upload

---
+++ Averaging Data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+################################################################
+InputFileName='https://media.githubusercontent.com/media/Apress/practical-data-science/master/VKHCG/01-Vermeulen/00-RawData/IP_DATA_CORE.csv'
+OutputFileName=r'C:\Users\girish.pandey\Desktop\DS Pract\Retrieve_Router_Location.csv'
+print('################################')
+print('Working Base :' ' using ')
+print('################################')
+sFileName=r'https://media.githubusercontent.com/media/Apress/practical-data-science/master/VKHCG/01-Vermeulen/00-RawData/IP_DATA_CORE.csv'
+print('Loading :',sFileName)
+IP_DATA_ALL=pd.read_csv(sFileName,header=0,low_memory=False,
+ usecols=['Country','Place Name','Latitude','Longitude'], encoding="latin-1")
+IP_DATA_ALL.rename(columns={'Place Name': 'Place_Name'}, inplace=True)
+AllData=IP_DATA_ALL[['Country', 'Place_Name','Latitude']]
+print(AllData)
+MeanData=AllData.groupby(['Country', 'Place_Name'])['Latitude'].mean()
+print(MeanData)
